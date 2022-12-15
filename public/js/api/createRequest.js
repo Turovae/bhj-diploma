77dd3c4f65f031c993944259e4b000ed3ac42f7b
Turovae <ekaterina.turova@mail.ru>
2022-12-15 12:20:30 +0700

    change createRequest module

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -5,44 +5,40 @@
 const createRequest = (options = {}) => {
   const xhr = new XMLHttpRequest();
   xhr.responseType = 'json';
+  
+  if (options.method === 'GET') {
+    let url = options.url;
 
-  let error;
-  let response;
-
-  try {
-
-    if (options.method === 'GET') {
-      let url = options.url;
-      if (options.data) {
-        url += '?';
-        for (let key in options.data) {
-          url += `${key}=${options.data[key]}&`;
-        }
-        url = url.substring(0, url.length - 1);
-      }
-      
-      xhr.open(options.method, url);
-      xhr.send();
-    } else {
-      const formData = new FormData();
-
+    if (options.data) {
+      url += '?';
       for (let key in options.data) {
-        formData.append(key, options.data[key]);
+        url += `${key}=${options.data[key]}&`;
       }
-      xhr.open(options.method, options.url);
-      xhr.send(formData);
+      url = url.substring(0, url.length - 1);
     }
 
-    xhr.addEventListener('readystatechange', () => {
-      if (xhr.readyState === xhr.DONE) {
-        response = xhr.response;
-        if (!response) {
-          error = "Неправильный запрос"
-        }
-        options.callback(error, response);
-      }
-    });
-  } catch (err) {
-    options.callback(err, null);
+    try {
+      xhr.open(options.method, url);
+      xhr.send();
+    } catch (err) {
+      options.callback(err, null);
+    }
+  } else {
+    const formData = new FormData();
+
+    for (let key in options.data) {
+      formData.append(key, options.data[key]);
+    }
+
+    try {
+      xhr.open(options.method, options.url);
+      xhr.send(formData);
+    } catch (err) {
+      options.callback(err, null);
+    }
   }
+
+  xhr.addEventListener('load', () => {
+    options.callback(xhr.error, xhr.response)
+  })
 };
